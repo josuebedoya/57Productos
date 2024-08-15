a02271f68217962ef3587caf8d83d2d42066aafc
josuebedoya <josuebedoya348@gmail.com>
2024-08-15 18:14:25 -0500

    created slider

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,14 +1,22 @@
 import { Header } from '../Components/Header';
+import { Slider } from '../Components/Slider/Slider'
 
 const Home = () => {
-  
+
+  const titleSlider = 'Tienda Online';
+
   return (
+
     <>
-    <div className='mb-12'>
-    <Header />
-    </div>
+      <div className='mb-12'>
+        <Header />
+
+        <Slider title={titleSlider}>
+          !Has crecer tus ganancias con nuestra ayuda¡
+        </Slider>
+      </div>
     </>
   )
 }
 
-export {Home};
+export { Home };
