1ec0bf79695abea3a37d7924fbba8f5756519a51
josuebedoya <josuebedoya348@gmail.com>
2024-09-17 11:41:07 -0500

    Add Page profile

--- src/Layout/Profile.jsx
+++ src/Layout/Profile.jsx
@@ -1,7 +1,7 @@
 const Profile = () =>{
   return (
   <div className='flex items-center justify-center bg-gray-200 text-green-700 text-3xl'>
-     pagina Contacto
+     Este es tu perfil
   </div>
   )
 }
