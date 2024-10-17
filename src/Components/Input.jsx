491cdcf04e582df40f638db629f0fddda6de5786
josuebedoya348 <josuebedoya348@gmail.com>
2024-10-16 23:04:17 -0500

    Add class Label and name

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Input = ({ value, onChange, placeholder, type, name, maxLength = 20, isRequired = false, label, children }) => {
     return (
-        <div className='input w-full' >
-            {label ? <label>{children}</label> : ''}
+        <div className='input w-full flex-col' >
+            {label ? <label htmlFor={name} className='label-input'>{children}</label> : ''}
             <input
                 type={type}
                 maxLength={maxLength}
