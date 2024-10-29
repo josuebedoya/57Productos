22f0795937c1286cd12b495328cf7bc93b5c116a
josuebedoya <josuebedoya348@gmail.com>
2024-10-29 13:06:21 -0500

    ↔ Main section, remove money to new component

--- src/Templates/Profile-parts/MainSection.jsx
+++ src/Templates/Profile-parts/MainSection.jsx
@@ -1,48 +1,40 @@
 import { useState, useEffect } from "react";
 import { EyeClose, EyeOpen } from "../../Resources/Icons";
+import { Money } from "../../Components/Money";
 
 const MainSectionProfile = () => {
 
-  let amount = '100034.345';
-  const [ showMoney, setShowMoney ] = useState( true );
-  const [ money, setMoney ] = useState( amount );
-  const data = () => {
-    return Intl.NumberFormat( 'es-ES', {
-      style: 'currency',
-      currency: 'COP',
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2
-    } ).format( money )
-  };
+  let amount = '10000000';
+  const [ showMoney, setShowMoney ] = useState(true);
+  const [ money, setMoney ] = useState(amount);
 
-  useEffect( () => {
-    setMoney( amount );
-  }, [ amount ] );
+  useEffect(() => {
+    setMoney(amount);
+  }, [ amount ]);
 
-  const hiddenAmountMoney = ( Amount ) => {
-    setShowMoney( !showMoney );
-    setMoney( () => '*'.repeat( Amount.length ) );
+  const hiddenAmountMoney = (Amount) => {
+    setShowMoney(!showMoney);
+    setMoney(() => '*'.repeat(Amount.length));
   };
 
   return (
     <>
       <section id="SectionProfile">
         <div className='container mx-auto'>
-          <div className="section-info flex justify-between">
-            <div className="user-content">
+          <div className="section-info flex flex- justify-between">
+            <div className="user-content w-auto">
               <h1 className="text-center">Tu Perfil</h1>
               <p className="text-center">@josuebedoya348</p>
             </div>
-            <div>
-              <div className='flex justify-center'>
-                <h3>Tu saldo</h3>
-                <i onClick={ () => hiddenAmountMoney( money ) }>
-                  { showMoney ? <EyeClose/> : <EyeOpen/> }
+            <div className=" w-44">
+              <div className='flex justify-between items-center gap-4 '>
+                <h3>Tu saldo:</h3>
+                <i onClick={() => hiddenAmountMoney(money)}>
+                  {showMoney ? <EyeClose /> : <EyeOpen />}
                 </i>
               </div>
-              <p className='amountMoney'>
-                { !showMoney ? money : data() }
-                {/*${ !showMoney ? money : amount.slice(0, 10) + '...'}*/ }
+              <p>
+                {!showMoney ? money : <Money amount={amount} />}
               </p>
             </div>
           </div>
