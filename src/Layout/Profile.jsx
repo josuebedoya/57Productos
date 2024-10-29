2510bb94cd212a941b96459b2d1616e6de8839f1
josuebedoya <josuebedoya348@gmail.com>
2024-10-29 18:04:42 -0500

    ↔ Use component Divisas... TEST

--- src/Layout/Profile.jsx
+++ src/Layout/Profile.jsx
@@ -1,23 +1,39 @@
 import { useState } from "react";
 import { GuestSection } from "../Templates/Profile-parts/GuestSection";
 import { MainSectionProfile } from "../Templates/Profile-parts/MainSection";
+import { Divisas } from "../Components/Divisas";
 
 const Profile = () => {
 
-  const [ loged, setLoget ] = useState( false );
-
+  const [ loged, setLoget ] = useState(false);
+  const [ rates, setRates ] = useState({
+    COP: null,
+    USD: null,
+    EUR: null,
+    GBP: null,
+    JPY: null,
+  });
   const handleLoged = () => {
-    setLoget( !loged );
+    setLoget(!loged);
   };
 
   return (
     <>
-      <MainSectionProfile/>
-      {/*{loged ? (*/ }
-      {/*  <MainSectionProfile />*/ }
-      {/*) : (*/ }
-      {/*  <GuestSection isLogin={handleLoged} />*/ }
-      {/*)}*/ }
+      <div className="container mx-auto px-16">
+        <MainSectionProfile />
+        <Divisas setRates={setRates} /> {/* Pasar la función setRates como prop */}
+        {
+          <div>
+            <p>100 USD a COP: {rates.COP ? (10000000 * rates.COP).toFixed(2) : "N/A"}</p>
+          </div>
+        }
+      </div>
+
+      {/*{loged ? (*/}
+      {/*  <MainSectionProfile />*/}
+      {/*) : (*/}
+      {/*  <GuestSection isLogin={handleLoged} />*/}
+      {/*)}*/}
     </>
   );
 };
