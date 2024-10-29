80216eeb1333811284cd578c04f1b8492c373728
josuebedoya348 <josuebedoya348@gmail.com>
2024-10-28 23:15:35 -0500

    Advanced MainSectionProfile, add price with money global

--- src/Templates/Profile-parts/MainSection.jsx
+++ src/Templates/Profile-parts/MainSection.jsx
@@ -1,12 +1,57 @@
+import { useState, useEffect } from "react";
+import { EyeClose, EyeOpen } from "../../Resources/Icons";
+
 const MainSectionProfile = () => {
-    return (
-        <>
-            <div id="SectionProfile" className="py-20">
-                <h1 className="text-center text-5xl">Tu Perfil</h1>
-                <p className="text-center text-2xl">Aquí podrás encontrar toda tu informción</p>
+
+  let amount = '100034.345';
+  const [ showMoney, setShowMoney ] = useState( true );
+  const [ money, setMoney ] = useState( amount );
+  const data = () => {
+    return Intl.NumberFormat( 'es-ES', {
+      style: 'currency',
+      currency: 'COP',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    } ).format( money )
+  };
+
+  useEffect( () => {
+    setMoney( amount );
+  }, [ amount ] );
+
+  const hiddenAmountMoney = ( Amount ) => {
+    setShowMoney( !showMoney );
+    setMoney( () => '*'.repeat( Amount.length ) );
+  };
+
+  return (
+    <>
+      <section id="SectionProfile">
+        <div className='container mx-auto'>
+          <div className="section-info flex justify-between">
+            <div className="user-content">
+              <h1 className="text-center">Tu Perfil</h1>
+              <p className="text-center">@josuebedoya348</p>
             </div>
-        </>
-    );
+            <div>
+              <div className='flex justify-center'>
+                <h3>Tu saldo</h3>
+                <i onClick={ () => hiddenAmountMoney( money ) }>
+                  { showMoney ? <EyeClose/> : <EyeOpen/> }
+                </i>
+              </div>
+              <p className='amountMoney'>
+                { !showMoney ? money : data() }
+                {/*${ !showMoney ? money : amount.slice(0, 10) + '...'}*/ }
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
-export { MainSectionProfile };
+export {
+  MainSectionProfile
+};
