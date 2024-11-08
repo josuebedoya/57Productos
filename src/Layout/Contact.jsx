53609f2e6bf15671a95b9ae044e644b0b7386c25
josuebedoya <josuebedoya348@gmail.com>
2024-11-08 12:41:38 -0500

    get database to sub-modules

--- src/Layout/Contact.jsx
+++ src/Layout/Contact.jsx
@@ -1,8 +1,37 @@
-const Contact = () =>{
+import React, { useEffect, useState } from 'react';
+import { GetProducts } from 'base-datos/src';
+
+function Contact() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const productsData = await GetProducts();
+        setProducts(productsData);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (error) {
+    return <p>Algo ha fallado: {error}</p>;
+  }
+
   return (
-  <div className='flex items-center justify-center bg-gray-200 text-green-700 text-3xl'>
-     pagina Contacto
-  </div>
-  )
+    <div>
+      <h1>Productos</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>{product.nombre}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
 export {Contact};
