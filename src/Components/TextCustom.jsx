e0bb9f671f546365eb74ae36f5ca8d6e497ce0e8
josuebedoya348 <josuebedoya348@gmail.com>
2024-11-22 00:09:45 -0500

    Finish Template Chance |+ news vars to tailwind | ↔ Button, TextCustom

--- src/Components/TextCustom.jsx
+++ src/Components/TextCustom.jsx
@@ -1,26 +1,32 @@
 const TextCustom = ({ title, children, lineTitle, atrTitle, titlePosition, atrsummary, linePosition }) => {
   return (
     <>
-      <div id='TextCustom'>
+      <div className='text-custom'>
         <div className='container mx-auto'>
           <div className='tilte-section' style={{ textAlign: titlePosition }}>
-            <h2 className={`title-custom 
-              ${lineTitle && linePosition === 'start' ? 'TitleWithLineStart' :
-              lineTitle && linePosition === 'center' ? 'TitleWithLineCenter' :
-                lineTitle && linePosition === 'end' ? 'TitleWithLineEnd' :
-                    ''
-              }
-            `}
-              style={atrTitle}>
-              {title}
-            </h2>
+            {
+              title ?
+                <h2 className={`title 
+                  ${lineTitle && linePosition === 'start' ? 'TitleWithLineStart' :
+                    lineTitle && linePosition === 'center' ? 'TitleWithLineCenter' :
+                      lineTitle && linePosition === 'end' ? 'TitleWithLineEnd' :
+                        ''
+                  }
+                `}
+                  style={atrTitle}>
+                  {title}
+                </h2> : null
+            }
           </div>
           <div className='summary-section'>
-            <div className='summary'>
-              <p style={atrsummary}>
-                {children}
-              </p>
-            </div>
+            {
+              children ?
+                <div className='summary'>
+                  <p style={atrsummary}>
+                    {children}
+                  </p>
+                </div> : null
+            }
           </div>
         </div>
       </div>
