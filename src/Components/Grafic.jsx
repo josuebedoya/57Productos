8dfbdbe3cac4cd1c3981eee877e287761ea6e9b8
josuebedoya348 <josuebedoya348@gmail.com>
2024-11-23 00:20:09 -0500

    ↔ Use Icons in Grafic

--- src/Components/Grafic.jsx
+++ src/Components/Grafic.jsx
@@ -1,3 +1,5 @@
+import {AddCircleIcon, CartIcon, TicketMoney, Plant, HandEstrech} from'../Resources/Icons';
+
 const Grafic = () => {
   return (
     <>
@@ -5,14 +7,19 @@ const Grafic = () => {
         <div className=' flex justify-center items-end w-auto'>
           <div class='grafic-content h-52 w-52 inline-flex gap-3 justify-center items-end duration-800 bg-gray-200 border-x-2 border-x-gray-50 border-y-2 border-y-blue-800 py-12 px-8 rounded-full hover:border-y-gray-50 hover:border-x-blue-800 hover:cursor-pointer'>
             <div className='bar barOne w-2.5 bg-transparent border border-purple-800'>
+              <AddCircleIcon classIcons='icon'/>
             </div>
             <div className='bar barTwo w-2.5 bg-transparent border border-amber-400'>
+            <CartIcon classIcons='icon'/>
             </div>
             <div className='bar barThree w-2.5 bg-transparent border border-blue-600'>
+            <TicketMoney classIcons='icon'/>
             </div>
             <div className='bar barFour w-2.5 bg-transparent border border-red-600'>
+            <Plant classIcons='icon'/>
             </div>
             <div className='bar barFive w-2.5 bg-transparent border border-green-600'>
+            <HandEstrech classIcons='icon'/>
             </div>
           </div>
         </div>
