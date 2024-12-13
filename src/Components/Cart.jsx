ca0b1372cde7fe6346e83e8b3fe25d24d3a2e74e
josuebedoya <josuebedoya348@gmail.com>
2024-12-13 17:24:22 -0500

    - Spaces class

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { CartIcon } from '../Resources/Icons';
 
 const Cart = () => {
-    const [counter, setCounter] = useState(0);
+    const [ counter, setCounter ] = useState(0);
 
     const AddCounter = () => {
         if (counter <= 99) {
@@ -12,47 +12,11 @@ const Cart = () => {
 
     return (
         <div id='CartDropdown' onClick={AddCounter}>
-            <div className='cart 
-                flex items-center 
-              text-white bg-Primary
-                border border-Primary
-                rounded-full
-                hover:bg-transparent
-              hover:text-Secondary
-                hover:border
-              hover:border-Secondary
-                hover:font-semibold 
-                duration-150
-                hover:cursor-pointer
-                relative'
-            >
+            <div className='cart flex items-center text-white bg-Primary border border-Primary rounded-full hover:bg-transparent hover:text-Secondary hover:border hover:border-Secondary hover:font-semibold duration-150 hover:cursor-pointer relative'>
                 <CartIcon />
-
                 {counter > 0 && (
-                    <div className='absolute
-                        top-five
-                        right-five
-                        flex
-                        items-center
-                        justify-center
-                        translate-x-1/2
-                        -translate-y-1/2'
-                    >
-                        <span className='text-13
-                            bg-white
-                            text-Primary
-                            rounded-full 
-                            p-1
-                            min-w-6
-                            min-h-6
-                            max-h-6
-                            border-2
-                            border-Gray-dark
-                            flex
-                            items-center
-                            justify-center
-                            text-center'
-                        >
+                    <div className='absolute top-five right-five flex items-center justify-center translate-x-1/2 -translate-y-1/2'>
+                        <span className='text-13 bg-white text-Primary rounded-full p-1 min-w-6 min-h-6 max-h-6 border-2 border-Gray-dark flex items-center justify-center text-center'>
                             {counter <= 99 ? counter : '99+'}
                         </span>
                     </div>
