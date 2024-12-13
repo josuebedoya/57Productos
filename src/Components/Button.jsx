a6c7a041027de685645a8d20dc10e2807194b71d
josuebedoya <josuebedoya348@gmail.com>
2024-12-13 15:14:27 -0500

    -  Spaces in class  from component button

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,10 +2,7 @@ function Button({ children, icon, iconRight, classBtn, ...FuctionButton }) {
   return (
     <>
       <button
-        className={`
-                    ${classBtn} btn-custom flex items-center gap-2 bg-Primary text-white font-semibold px-1.5 py-1.5 
-                    rounded-full shadow-md border border-primary hover:bg-transparent hover:text-Secondary hover:border hover:border-Secondary hover:font-semibold duration-300
-                  `}
+        className={`${classBtn} btn-custom flex items-center gap-2 bg-Primary text-white font-semibold px-1.5 py-1.5 rounded-full shadow-md border border-primary hover:bg-transparent hover:text-Secondary hover:border hover:border-Secondary hover:font-semibold duration-300`}
         {...FuctionButton}
       >
         {iconRight === true ? (
