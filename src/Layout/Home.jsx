9e13368a6d2def0ea7f92422b6a2ccf7f31371ee
josuebedoya <josuebedoya348@gmail.com>
2024-12-13 12:24:21 -0500

    + section text h1

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,24 +1,26 @@
 import { Slider } from '../Components/Slider';
 import { Blog } from '../Templates/Blog';
 import { Chance } from '../Templates/Chance';
+import { HomeTextMain } from '../Templates/H1-home';
 import { OpinionItem } from '../Templates/Parts/OpinionsItem';
 
 const Home = () => {
 
   const titleSlider = 'Tienda Online';
-  
+
   return (
     <>
       <div className='mb-12'>
         <OpinionItem name='Programer'>
-        Yo opino qué, no se que opinar, esta es una opinion donde se opina,{'\n'}
-        pero entonces no se que opinar, yo creo que toca opinar algo, pero ese algo hay que saber opinarlo!
+          Yo opino qué, no se que opinar, esta es una opinion donde se opina,{'\n'}
+          pero entonces no se que opinar, yo creo que toca opinar algo, pero ese algo hay que saber opinarlo!
         </OpinionItem>
         <Slider title={titleSlider}>
           !Has crecer tus ganancias con nuestra ayuda¡
         </Slider>
-        <Blog/>
+        <Blog />
         <Chance />
+        <HomeTextMain />
       </div>
     </>
   )
