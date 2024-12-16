d2669a3c3d1f354be65b443352d34e4e185cb3d6
josuebedoya <josuebedoya348@gmail.com>
2024-12-16 16:39:23 -0500

    + validation to button text

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,8 +1,8 @@
-function Button({ children, icon, iconRight, classBtn, ...FuctionButton }) {
+function Button({ children, icon, iconRight, classBtn, btnText, ...FuctionButton }) {
   return (
     <>
       <button
-        className={`${classBtn} btn-custom flex items-center gap-2 bg-Primary text-white font-semibold px-1.5 py-1.5 rounded-full shadow-md border border-primary hover:bg-transparent hover:text-Secondary hover:border hover:border-Secondary hover:font-semibold duration-300`}
+        className={`${classBtn} btn-custom flex items-center gap-2 ${btnText ? 'bg-transparent text-Primary duration-150 ': 'bg-Primary text-white border border-primary hover:bg-transparent hover:border hover:border-Secondary shadow-md rounded-full px-1.5 py-1.5 duration-300'} hover:text-Secondary font-semibold hover:font-semibold`}
         {...FuctionButton}
       >
         {iconRight === true ? (
