c0b1b90c1e30cf0f9f9b67007fdc9a96e35b6cd3
josuebedoya <josuebedoya348@gmail.com>
2025-01-20 14:37:28 -0500

    Migrate to vite V-3.2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { App } from './App.jsx'
-import '/src/styles/_glogal.scss'
 import './main.css'
 import { CartProvider } from './context/cart.jsx'
 import { CommentProvider } from "@/context/comments.jsx"
