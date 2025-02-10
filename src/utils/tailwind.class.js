8c53f02a1849eb12cd7e25c932c3aabc39df5a6c
josuebedoya348 <josuebedoya348@gmail.com>
2025-02-09 21:55:46 -0500

    + breakpoints to class safelist

--- src/utils/tailwind.class.js
+++ src/utils/tailwind.class.js
@@ -4,6 +4,7 @@
  */
 
 const colours = [ 'blue', 'red', 'yellow', 'purple','green', 'cyan', 'fuchsia', 'pink', 'orange'];
+const breakpoints = [ 'mn:', 'xn:', 'sm:', 'md:', 'tl:', 'lg:', 'xl:', '2xl:' ];
 
 export default function CSafelist() {
   const safelist = [ 
@@ -11,6 +12,7 @@ export default function CSafelist() {
     [...Array(20)].map((_, i) => `gap-${i + 1}`), // ['gap-1', ...]
     colours.map(color => `shadow-${color}-500`,color => `hover:shadow-${color}-500`),// ['shadow-blue-500',...]
     colours.map(color => `hover:shadow-${color}-500`),// ['hover:shadow-blue-500',...]
+    breakpoints.flatMap(( b ) => [...Array(12)].map((_, i) => `${ b }grid-cols-${i + 1}`)), // ['mn:grid-cols-1'...]
   ].flat(); // Join Arrays
 
   return safelist;
