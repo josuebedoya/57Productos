5407df18eb320993d1e845c553228f5972816c34
josuebedoya <josuebedoya348@gmail.com>
2025-02-18 09:50:30 -0500

    ↔ useComment

--- src/context/comments.jsx
+++ src/context/comments.jsx
@@ -1,33 +1,26 @@
 import { createContext, useState, useContext, useEffect } from 'react';
-import { insertData, getData, updateData } from 'management-supabase';
+import { updateData } from 'management-supabase';
+import  { useDatabase } from "@/utils/database.jsx";
 
 const CommentContext = createContext();
 
 const CommentProvider = ( { children } ) => {
   const [ comments, setComments ] = useState( [] );
   const [ localComments, setLocalComments ] = useState( [] );
-  const [ error, setError ] = useState( null );
+  const  { data, getData, insertData} =  useDatabase();
 
   useEffect( () => {
-    const fetchComments = async () => {  // get Comments
-      try {
-        const res = await getData( 'comments_users' );
-        setComments( res );
-      } catch ( err ) {
-        setError( err.message );
-      }
-    };
-    fetchComments();
-  }, [] );
+  getData( 'comments_users' );
+  }, [] ); // get Comments
 
-  const addMessage = async ( message, name ) => { // add new comment
+  useEffect( () => {
+    setComments(data);
+  }, [data] ); // update comments
+
+  const addMessage = ( message, name ) => { // add new comment
     if ( message && name ) {
-      try {
-        await insertData( 'comments_users', { name: name, comment: message } );
-        setLocalComments( prevComment => [ ...prevComment, { name: name, comment: message, like: 0, dont_like: 0 } ] )
-      } catch ( err ) {
-        setError( err.message );
-      }
+      insertData( 'comments_users', { name: name, comment: message } );
+        setLocalComments( prevComment => [ ...prevComment, { name: name, comment: message, like: 0, dont_like: 0 } ] );
     }
   };
 
@@ -42,11 +35,6 @@ const CommentProvider = ( { children } ) => {
       return <div>Algo ha fallado: { error.message }</div>;
     }
   };
-
-  if ( error ) {
-    console.log( error.message )
-    return <div>Algo ha fallado: { error.message }</div>;
-  }
 
   return (
    <CommentContext.Provider value={ { localComments, comments, addMessage, rateLike } }>
