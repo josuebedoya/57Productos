62b9650e13dc5e5abbbd86bd2343de13a1c5cc3a
josuebedoya <josuebedoya348@gmail.com>
2025-03-17 18:36:11 -0500

    + variable Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -54,6 +54,7 @@ module.exports = {
         '105': '420px',
         '110': '440px',
         '103': '412px',
+        'full-vh': '100vh'
       },
       maxWidth: {
         '93': '930px',
@@ -70,8 +71,8 @@ module.exports = {
         '38': '150px',
       },
       animation: {
-        'fade-out': 'fade-out .2s ease-in both',
-        'fade-in': 'fade-in .2s ease-in both',
+        'fade-out': 'fade-out .4s ease-in both',
+        'fade-in': 'fade-in .4s ease-in both',
         'shaking': 'shaking  2.5s linear infinite',
         'collapse-top-in': 'collapse-top-in .5s ease-in both',
         'collapse-top-out': 'collapse-top-out .5s ease-out both'
