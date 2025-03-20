fb8973492d91b352c339ddfeddeac07582cf1b2b
josuebedoya <josuebedoya348@gmail.com>
2025-03-20 11:03:28 -0500

    + Context to handle params url

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import { App } from './App.jsx'
 import './main.css'
 import { CartProvider } from './context/cart.jsx'
 import { CommentProvider } from "@/context/comments.jsx"
+import { ParamsUrlProvider } from '@/context/ParamsUrl.jsx'
 
 createRoot( document.getElementById( 'root' ) ).render( <StrictMode>
-  <CommentProvider>
-    <CartProvider>
-      <Router>
-        <App/>
-      </Router>
-    </CartProvider>
-  </CommentProvider>
+  <Router>
+    <CommentProvider>
+      <ParamsUrlProvider>
+        <CartProvider>
+          <App/>
+        </CartProvider>
+      </ParamsUrlProvider>
+    </CommentProvider>
+  </Router>
 </StrictMode> );
