1c55787dddbe423a216dd43803fe1216a50a23f4
josuebedoya348 <josuebedoya348@gmail.com>
2025-09-01 15:34:13 -0500

    + Add checkbox input

---
+++ src/components/input/fields/input/types/checkbox.tsx
@@ -0,0 +1,54 @@
+import React, {useEffect, useState} from 'react';
+import type {InputProps} from "../types.ts";
+import {gVar} from "@/utils/gVar.js";
+import {MarkIcon} from '@/assets/icons.tsx'
+
+const CheckBox = (props: InputProps) => {
+
+  const [checkboxStyles, setCheckboxStyles] = useState('');
+  const [checked, setChecked] = useState(false);
+  const [iconColor, setIconColor] = useState('text-Primary');
+
+  const {
+    color = 'primary',
+    rounded = 'md',
+    variant = 'solid',
+    padding = 'md',
+  } = props || {};
+
+  // Update style class
+  useEffect(() => {
+    const checkboxStyles = gVar([
+      'input.base',
+      `rounded.${rounded}`,
+      `input.checkbox.padding.${padding}`,
+      `input.checkbox.variant.${variant}.${color}`,
+      `input.checkbox.base`
+    ]);
+
+    setCheckboxStyles(checkboxStyles);
+  }, [props]);
+
+  // Update icon color
+  useEffect(() => {
+    const pathStyles = !checked || variant !== 'solid' ? `text.color.${color}` : `input.checkbox.variant.solid.icon.${color}`;
+
+    setIconColor(gVar(pathStyles));
+  }, [props]);
+
+  return (
+    <div className="relative flex justify-center items-center w-full h-auto">
+      <input
+        type='checkbox'
+        className={`checkbox ${checkboxStyles} ${props.className}`}
+        onClick={() => setChecked(!checked)}
+        {...props}
+      />
+      <span className={`check-icon absolute text-center pointer-events-none duration-300 ${iconColor}`}>
+        {props.iconCheckbox ? props.iconCheckbox : <MarkIcon/>}
+      </span>
+    </div>
+  );
+};
+
+export default CheckBox;
