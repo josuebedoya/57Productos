2f55246cde72ade2eb5f896edb991eb00347ee4f
josuebedoya348 <josuebedoya348@gmail.com>
2025-09-01 18:36:40 -0500

    +  Add range input

---
+++ src/components/input/fields/input/types/range.tsx
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from 'react';
+import type {InputProps} from "@/components/input/fields/input/types.js";
+import {gVar} from "@/utils/gVar.js";
+
+const Range: React.FC<InputProps> = ({className = '', ...props}) => {
+
+  const [rangeStyles, setRangeStyles] = useState('');
+
+  const {
+    color = 'primary',
+    rounded = 'md',
+    variant = 'solid',
+    min = 0,
+    max = 100,
+    step = 5,
+    value = Math.floor((Number(props.max) || 0) / 2)
+  } = props || {};
+
+  // Update style class
+  useEffect(() => {
+    const checkboxStyles = gVar([
+      'input.range.base',
+      `rounded.${rounded}`,
+      `input.range.variant.${variant}.${color}`,
+    ]);
+
+    setRangeStyles(checkboxStyles);
+  }, [props]);
+
+  return (
+    <input
+      name={props.name ?? 'range'}
+      type='range'
+      className={`range ${rangeStyles} ${className}`}
+      min={min}
+      max={max}
+      step={step}
+      value={value}
+      {...props}
+    />
+  );
+};
+
+export default Range;
