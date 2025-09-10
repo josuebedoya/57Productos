e1c8daafc7d73e67ebeb9451f979e8f526f93a55
josuebedoya348 <josuebedoya348@gmail.com>
2025-09-10 09:39:29 -0500

    + Add label to select

--- src/components/input/fields/select/index.tsx
+++ src/components/input/fields/select/index.tsx
@@ -7,6 +7,9 @@ const Select: React.FC<SelectProps> = (
   {
     options = [],
     name = 'select',
+    label,
+    labelClassName = '',
+    withLabel,
     defaultValue = 0,
     className,
     icon,
@@ -35,37 +38,45 @@ const Select: React.FC<SelectProps> = (
   }, [rest]);
 
   return (
-    <div className='relative flex justify-between items-center w-full h-auto'>
-      <select
-        name={name}
-        defaultValue={defaultValue}
-        className={`controller select-${name} order-0 ${selectStyles} ${className}`}
-        onChange={onChange}
-        onFocus={() => setOpen(true)}
-        onBlur={() => setOpen(false)}
-        {...rest}
-      >
-        {options.map(({value, label, disabled, ...rest}: OptionsProps, index: number) => (
-          <option
-            id={index.toString()}
-            key={index}
-            value={value}
-            disabled={disabled}
-            {...rest}
-          >
-            {label ?? value}
-          </option>
-        ))}
-      </select>
-      {
-        !rest?.multiple && <span
-        className={`right-2 order-1 absolute ${open ? 'rotate-90' : 'rotate-0'} !w-auto ${selectStyles}`}>
+    <>
+      {(label && withLabel) && (
+        <label className={`label ${labelClassName}`} htmlFor={name}>
+          {label}
+        </label>
+      )}
+      <div className='relative flex justify-between items-center w-full h-auto'>
+        <select
+          name={name}
+          defaultValue={defaultValue}
+          className={`controller select-${name} order-0 ${selectStyles} ${className}`}
+          onChange={onChange}
+          onFocus={() => setOpen(true)}
+          onBlur={() => setOpen(false)}
+          {...rest}
+        >
+          {options.map(({value, label, disabled, ...rest}: OptionsProps, index: number) => (
+            <option
+              id={index.toString()}
+              key={index}
+              value={value}
+              disabled={disabled}
+              {...rest}
+            >
+              {label ?? value}
+            </option>
+          ))}
+        </select>
+        {
+          !rest?.multiple && <span
+          className={`right-2 order-1 absolute ${open ? 'rotate-90' : 'rotate-0'} !w-auto ${selectStyles}`}>
         {
           icon ? icon : <AngleRightIcon/>
         }
       </span>
-      }
-    </div>
+        }
+      </div>
+    </>
+
   );
 };
 
