9520794be51154a2242098a1e2a69787c830e51d
josuebedoya348 <josuebedoya348@gmail.com>
2025-09-30 14:57:24 -0500

    + Added default values file

---
+++ src/components/media/configs/defaultContent.ts
@@ -0,0 +1,11 @@
+import {useMemo} from "react";
+
+export const defaultsImg = {
+  notfound: '/assets/images/system/not-found.png',
+  unsupported: '/assets/images/system/unsupported.png',
+  unknown: '/assets/images/system/unsupported.png',
+  default: '/assets/images/system/default-image.webp',
+  image: ''
+};
+
+export const defaultExt = useMemo(() => ['png', 'jpeg', 'jpg', 'mp4', 'mov', 'pdf'], []);
