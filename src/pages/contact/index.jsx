d01b583ea551896125b25609fd92157bf490abcf
josuebedoya348 <josuebedoya348@gmail.com>
2025-10-21 10:16:50 -0500

    + Add animations to tailwind.config

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,21 +1,35 @@
 import { useSettings } from '@/context/settings.jsx';
 import { Metas } from '@/components/metas/metas.jsx';
-import NumberControl from "@/components/numberControl/index.js";
+import Modal from "@/components/modal/index.tsx";
+import { useState } from "react";
+import Code from "@/components/code/index.js";
 
 const Contact = () => {
   const { settings } = useSettings();
 
+  const [ open, setOpen ] = useState( false );
+
   return (
-   <>
+   <div className='flex justify-center flex-col items-center my-10'>
      <Metas
       title={ `${ settings?.site.name } | Contacto` }
       description='Contactanos, y dejános tus datos, te contactaremos lo más rápido posible.'
       type='website'/>
      <section className='container mx-auto'>
        <h1>Asesorate, deja tus datos y te contactaremos los más rápido posible</h1>
-       <NumberControl/>
+       <Modal
+        type='drawer'
+        onClose={ () => setOpen( false ) }
+        isOpen={ open }
+        position='left'
+       />
+       <Code>HOLOAAAAAAAAAAAAAAA</Code>
      </section>
-   </>
+     <button className='p-10 text-white bg-green-400 rounded-full text-xl font-semibold text-center'
+             onClick={ () => setOpen( true ) }
+     >Abrir Modal
+     </button>
+   </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -79,9 +79,13 @@ module.exports = {
         'collapse-top-in': 'collapse-top-in .5s ease-in both',
         'collapse-top-out': 'collapse-top-out .5s ease-out both',
         'fade-up-in': 'fade-up-in .53s ease-in both',
-        'fade-down-in': 'fade-down-in .53 ease-in both',
         'fade-up-out': 'fade-up-out .53s ease-in both',
-        'fade-down-out': 'fade-down-out .53 ease-in both'
+        'fade-down-in': 'fade-down-in .53s ease-in both',
+        'fade-down-out': 'fade-down-out .53 ease-in both',
+        'fade-left-out': 'fade-left-out .53s ease-in both',
+        'fade-left-in': 'fade-left-in .53s ease-in both',
+        'fade-right-out': 'fade-right-out .53s ease-in both',
+        'fade-right-in': 'fade-right-in .53s ease-in both',
       },
       keyframes: {
         'fade-out': {
@@ -132,6 +136,22 @@ module.exports = {
         'fade-down-out': {
           '0%': { transform: 'translateY(0)', opacity: 1 },
           '100%': { transform: 'translateY(-70px)', opacity: 0 }
+        },
+        'fade-left-out': {
+          '0%': { transform: 'translateX(0)', opacity: 1 },
+          '100%': { transform: 'translateX(70px)', opacity: 0 }
+        },
+        'fade-left-in': {
+          '0%': { transform: 'translateX(-70px)', opacity: 0 },
+          '100%': { transform: 'translateX(0)', opacity: 1 }
+        },
+        'fade-right-out': {
+          '0%': { transform: 'translateX(0)', opacity: 1 },
+          '100%': { transform: 'translateX(-70px)', opacity: 0 }
+        },
+        'fade-right-in': {
+          '0%': { transform: 'translateX(70px)', opacity: 0 },
+          '100%': { transform: 'translateX(0)', opacity: 1 }
         }
       },
     },
