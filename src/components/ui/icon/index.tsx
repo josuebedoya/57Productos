d94dca9cca03b87d96a31b44fa059641d5a27ea6
josuebedoya348 <josuebedoya348@gmail.com>
2026-02-02 12:32:34 -0500

    |+| Added icons header

--- src/components/ui/icon/index.tsx
+++ src/components/ui/icon/index.tsx
@@ -1,8 +1,9 @@
-import { IconProps } from '@/resources/types';
-import { DynamicIcon } from 'lucide-react/dynamic';
+import {IconProps} from '@/resources/types';
+import {DynamicIcon} from 'lucide-react/dynamic';
+import {clsx} from "clsx";
 
-const Icon = ({ name, ...ui }: IconProps) => {
-  return <span className={ui.className}><DynamicIcon name={name} {...ui}/></span>;
+const Icon = ({name, ...ui}: IconProps) => {
+  return <span className={clsx('icon max-w-max max-h-max', ui.className)}><DynamicIcon name={name} {...ui}/></span>;
 };
 
 export default Icon;
--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -3,23 +3,38 @@ import Menu from "@ui/menu";
 import readMenu from "@/server/services/menu/readMenu";
 import Logo from "@/components/logo";
 import '@/styles/components/_header.scss';
+import Icon from "@ui/icon";
 
 const Header = async ({locale}: Locale) => {
   const {data: menuItems} = await readMenu(locale);
 
   return (
-    <header className='container mx-auto text-center flex'>
+    <header className='container mx-auto text-center flex items-center justify-between'>
       <div className='flex-initial py-3'>
         <Logo locale={locale}/>
       </div>
-      <div className='flex-1 self-center flex justify-end'>
+      <div className='flex-1 self-center flex justify-center items-center'>
         <Menu
           items={menuItems}
           dir='horizontal'
-          classNameItem='text-base xl:text-lg px-2 py-1'
+          classNameItem='text-base xl:text-lg px-2 py-1 hover:text-primary duration-200'
           classNameItemActive='font-bold text-primary'
           classNameIcon="mr-1.5 text-sm"
         />
+      </div>
+      <div className="controls flex items-center justify-end p-1 bg-primary rounded-xl max-h-max">
+        <Icon name='search'
+              strokeWidth={3}
+              width='20'
+              className='m-2 rounded-md text-white cursor-pointer font-semibold duration-200 hover:text-primary relative overflow-hidden'/>
+        <Icon name='user'
+              strokeWidth={3}
+              width='20'
+              className='m-2 rounded-md text-white cursor-pointer font-semibold duration-200 hover:text-primary relative overflow-hidden'/>
+        <Icon name='languages'
+              strokeWidth={3}
+              width='20'
+              className='m-2 rounded-md text-sm text-white cursor-pointer font-semibold duration-200 hover:text-primary relative overflow-hidden'/>
       </div>
     </header>
   );
